794495b4c6e29426ba1cc8694d4ef52c72f921f1
elihawley <104234916+elihawley@users.noreply.github.com>
2023-05-13 10:09:41 -0400

    Update Bat positions and movements

--- src/prefabs/Bat.js
+++ src/prefabs/Bat.js
@@ -2,15 +2,16 @@ class Bat extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y) {
         super(scene, x, y, 'bat');
         this.scale = 1.5;
-        this.angle = -.05 + Math.random() * .1;
-        this.moveSpeed = Math.random();
+        // this.angle = -.05 + Math.random() * .1;
+        this.moveSpeed = 1 + Math.random();
 
         this.scene = scene;
     }
 
     update() {
-        this.x += this.moveSpeed * Math.cos(180 + this.angle);
-        this.y += this.moveSpeed * Math.sin(this.angle);
+        // this.x += this.moveSpeed * Math.cos(180 + this.angle);
+        // this.y += this.moveSpeed * Math.sin(this.angle);
+        this.x -= this.moveSpeed;
 
         // reset on miss
         if (this.x <= borderUISize + borderPadding
@@ -23,8 +24,9 @@ class Bat extends Phaser.GameObjects.Sprite {
 
     resetToSpawnArea() {
         this.x = game.config.width - borderUISize - borderPadding;
-        this.y = 2*borderUISize + 2*borderPadding + Math.random()*(game.config.height - 4*borderUISize - 2*borderPadding);
-        this.angle = -.05 + Math.random() * .1;
-        this.moveSpeed = Math.random() * this.scene.speedMultiplier;
+        this.y = (Math.random() < .5) ? (4*borderUISize + 2*borderPadding) : (game.config.height - 2*borderUISize - 2*borderPadding);
+        // this.y = 2*borderUISize + 2*borderPadding + Math.random()*(game.config.height - 4*borderUISize - 2*borderPadding);
+        // this.angle = -.05 + Math.random() * .1;
+        this.moveSpeed = 1 + Math.random() * this.scene.speedMultiplier;
     }
 }
