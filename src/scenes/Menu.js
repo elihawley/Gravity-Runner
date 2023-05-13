6aa89d13d7f1e78ffd59a0cd310385f054bb9d9e
elihawley <104234916+elihawley@users.noreply.github.com>
2023-05-13 15:18:42 -0400

    Updated instructions text

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -26,7 +26,7 @@ class Menu extends Phaser.Scene {
 
 
         let t = this.add.text(game.config.width / 2, game.config.height / 3 - borderUISize * 2 - borderPadding * 2, 'GRAVITY RUNNER', menuConfig).setOrigin(.5);
-        let t1 = this.add.text(game.config.width / 2, game.config.height / 2, 'Use (G) to change gravity. Don\'t get COVID!', menuConfig).setOrigin(.5);
+        let t1 = this.add.text(game.config.width / 2, game.config.height / 2, 'Use (G) to change gravity. Avoid the bats!', menuConfig).setOrigin(.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
         this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize * 2 + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(.5);
