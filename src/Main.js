34c9f85c6451ef9ebd6daeabe2673be771b8007c
elihawley <104234916+elihawley@users.noreply.github.com>
2023-05-13 15:17:26 -0400

    Added comments to Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,11 @@
+/**
+ * Elia Hawley
+ * Gravity Runner
+ * Took around 27 hours to complete
+ * Creative tilt (technical): Added logic and hitboxes for stomping on bats with gravity and without gravity (see Play.js's checkCollision function). Took a lot of experimentation and turned out it could be done in a clean way.
+ * Creative tilt (style): Kept the style of Google Chrome's Dinosaur game but with more features (gravity control and squishing enemies) and animations (handcrafted sprites and explosions).
+ */
+
 let config = {
     type: Phaser.AUTO,
     width: 400,
