a9c7be81e6fd2515e2751a61f107fbb5bd4b8c8b
elihawley <104234916+elihawley@users.noreply.github.com>
2023-05-13 15:19:32 -0400

    Added bat animation

--- src/prefabs/BatGroup.js
+++ src/prefabs/BatGroup.js
@@ -7,7 +7,7 @@ class BatGroup extends Phaser.GameObjects.Group {
         this.maxSize = 30;
         this.numBats = 1;
 
-        this.createFromConfig({
+        let firstBat = this.createFromConfig({
             classType: Bat,
             quantity: 1,
             active: true,
@@ -17,7 +17,15 @@ class BatGroup extends Phaser.GameObjects.Group {
                 y: (Math.random() < .5) ? (4*borderUISize + 2*borderPadding) : (game.config.height - 2*borderUISize - 2*borderPadding),
             },
             key: 'bat'
+        })[0];
+
+        firstBat.anims.create({
+            key: 'anim_bat',
+            frames: this.scene.anims.generateFrameNumbers('bat', {start: 0, end: 1, first: 0}),
+            frameRate: 4,
+            repeat: -1,
         })
+        firstBat.play('anim_bat');
     }
 
     update() {
@@ -25,17 +33,11 @@ class BatGroup extends Phaser.GameObjects.Group {
         this.updateNumBats();
     }
 
-    // randomizePlacement() {
-    //     const spawnBounds = new Phaser.Geom.Rectangle(game.config.width - borderUISize, 2*borderUISize + 2*borderPadding, borderPadding, game.config.height - 4*borderUISize - 2*borderPadding);
-
-    //     Phaser.Actions.RandomRectangle(this.getChildren(), spawnBounds);
-    // }
-
     updateNumBats() {
         const currWaveDifficulty = this.scene.waveDifficultyLevel
         this.numBats = Math.min(30, Math.floor(currWaveDifficulty / 10));
         for (let i = 0; i < this.numBats - this.countActive(); i++) {
-            this.createFromConfig({
+            let newBat = this.createFromConfig({
                 classType: Bat,
                 quantity: 1,
                 active: true,
@@ -45,7 +47,15 @@ class BatGroup extends Phaser.GameObjects.Group {
                     y: (Math.random() < .5) ? (4*borderUISize + 2*borderPadding) : (game.config.height - 2*borderUISize - 2*borderPadding),
                 },
                 key: 'bat'
+            })[0];
+
+            newBat.anims.create({
+                key: 'anim_bat',
+                frames: this.scene.anims.generateFrameNumbers('bat', {start: 0, end: 1, first: 0}),
+                frameRate: 4,
+                repeat: -1,
             })
+            newBat.play('anim_bat');
         }
     }
 }
